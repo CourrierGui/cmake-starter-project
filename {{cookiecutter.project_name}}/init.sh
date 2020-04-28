9cb4b0daf0af1c85f95460559f8dff9b2c4d9210
Guillaume Courrier <guillaume.courrier@supelec.fr>
2020-04-28 21:23:32 +0200

    correct config + define remote git repo

--- {{cookiecutter.project_name}}/init.sh
+++ {{cookiecutter.project_name}}/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git init && git submodule update
+git init && git remote -v add origin {{cookiecutter.git_remote}} git submodule init && git submodule update
 mkdir -p build && cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. && ln -s compile_commands.json ../compile_commands.json
 
